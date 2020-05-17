c56661fc4eaf05bf8c7877d6780365d7d06ca5d5
Janiel Martell <martell.janiel@gmail.com>
2020-05-16 21:52:31 -0400

    Add projects section

--- .eleventy.js
+++ .eleventy.js
@@ -55,7 +55,7 @@ module.exports = function (config) {
     config.addLayoutAlias('resume', 'resume.njk')
 
     // Collections
-    const collections = ['work', 'education']
+    const collections = ['work', 'education', 'projects']
     collections.forEach((name) => {
         config.addCollection(name, function (collection) {
             const folderRegex = new RegExp(`\/${name}\/`)
